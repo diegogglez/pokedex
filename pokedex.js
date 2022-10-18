c4559ff6e1e600bf0cff34d40af85c25761df81b
diegogglez <diegomiller7@gmail.com>
2022-10-18 21:02:19 +0200

    Feat: adding styles and header elements

--- pokedex.js
+++ pokedex.js
@@ -25,12 +25,35 @@ document.body.appendChild(h2$$); */
 
 //* prueba con funciones
 
+
+const typeColors = {
+  fire: "#e50020",
+  grass: "#00a63c",
+  steel: "#00858a",
+  water: "#0050ac",
+  psychic: "#c90086",
+  ground: "#c90086",
+  ice: "#70deff",
+  flying: "#5d4e75",
+  ghost: "#4d5b64",
+  normal: "#753845",
+  poison: "#c94772",
+  rock: "#6e1a00",
+  fighting: "#634136",
+  dark: "#272625",
+  bug: "#6e1a00",
+  dragon: "#00c431",
+  electric: "#bba909",
+  fairy: "#d31c81",
+  unknow: "#757575",
+  shadow: "#29292c",
+}
 const pokedexGrid$$ = document.querySelector('#pokedex');
 const POKEMONS_INFO = [];
 
 //?Guardamos el primer fetch en la funcion getAllPokemos
 
- function getAllPokemons() {
+ async function getAllPokemons() {
   return fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
     .then((response) => response.json())
     .then((response) => {
@@ -39,7 +62,7 @@ const POKEMONS_INFO = [];
     .catch((error) => console.log('Error obteniendo todos los pokemons', error));
 }
 
- function getOnePokemon(url) {
+ async function getOnePokemon(url) {
   return fetch(url)
     .then((response) => response.json())
     .then((response) => {
@@ -48,30 +71,75 @@ const POKEMONS_INFO = [];
     .catch((error) => console.log('Error obteniendo pokemon individual', error));
 }
 
+function setTypeColors(p) {
+
+  p.style.backgroundColor = typeColors[p.textContent];
+
+}
+
 function renderPokedex(pokemons){
 
   for (let pokemon of pokemons){
     
     const card$$ = document.createElement('li');
+    const pickContainer$$ = document.createElement('div');
+    const infoContainer$$ = document.createElement('div');
     const img$$ = document.createElement('img');
     const name$$ = document.createElement('h2');
     const id$$ = document.createElement('p');
+    const typesContainer$$ = document.createElement('div')
+    //const type$$ = document.createElement('p');
 
     card$$.classList.add('card')
+    pickContainer$$.classList.add('pick-container');
+    infoContainer$$.classList.add('info-container');
+    img$$.classList.add('img');
+    typesContainer$$.classList.add('typesContainer')
+    id$$.classList.add('pokemonId')
   
-    img$$.src = pokemon.sprites.front_default;
+    img$$.src = pokemon.sprites.other.dream_world.front_default;
     name$$.textContent = pokemon.name;
-    id$$.textContent = pokemon.id;
+    id$$.textContent = `#${pokemon.id}`;
   
-    card$$.appendChild(img$$);
-    card$$.appendChild(name$$);
-    card$$.appendChild(id$$);
+    pickContainer$$.appendChild(img$$);
+    infoContainer$$.appendChild(id$$);
+    infoContainer$$.appendChild(name$$);
+    card$$.appendChild(pickContainer$$);
+    card$$.appendChild(infoContainer$$);
     pokedexGrid$$.appendChild(card$$);
+
+    for (let item of pokemon.types) {
+
+      const typeName$$ = document.createElement('p');
+      
+      typeName$$.classList.add('typeName')
+      typeName$$.textContent = item.type.name;
+
+      typesContainer$$.appendChild(typeName$$);
+      infoContainer$$.appendChild(typesContainer$$);
+
+      setTypeColors(typeName$$);
+    }
+
 
   }
 
 
 }
+//? Creando opciones para meter en la lista de tipos
+
+  for (let type in typeColors) {
+    /* console.log('Bucle forin',type); */
+    const typeOption$$ = document.createElement('option');
+    const typeSelector$$ = document.querySelector('.types-selector');
+    
+    typeOption$$.textContent = type;
+    typeOption$$.setAttribute('value', type);
+
+    typeSelector$$.appendChild(typeOption$$);
+
+  }
+
 
 
 async function init(){
@@ -88,6 +156,7 @@ async function init(){
   }
 
   renderPokedex(POKEMONS_INFO);
+
 }
 
 init()
