23bb9cb7861b06f33c5a518c5bbb7cb4a884cef7
diegogglez <diegomiller7@gmail.com>
2022-10-18 23:07:21 +0200

    Feat: adding name filter funtionality

--- pokedex.js
+++ pokedex.js
@@ -25,7 +25,6 @@ document.body.appendChild(h2$$); */
 
 //* prueba con funciones
 
-
 const typeColors = {
   fire: "#e50020",
   grass: "#00a63c",
@@ -48,13 +47,18 @@ const typeColors = {
   unknow: "#757575",
   shadow: "#29292c",
 }
+
 const pokedexGrid$$ = document.querySelector('#pokedex');
 const POKEMONS_INFO = [];
+const searcher$$ = document.querySelector('.searcher')
+const notFound$$ = document.createElement('h2');
+  notFound$$.textContent = 'Ese pokemon no existe...DE MOMENTO'
+  notFound$$.classList.add('notfound')
+  document.body.appendChild(notFound$$);
 
 //?Guardamos el primer fetch en la funcion getAllPokemos
-
- async function getAllPokemons() {
-  return fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+async function getAllPokemons() {
+  return fetch('https://pokeapi.co/api/v2/pokemon?limit=200')
     .then((response) => response.json())
     .then((response) => {
       return response.results;
@@ -62,7 +66,7 @@ const POKEMONS_INFO = [];
     .catch((error) => console.log('Error obteniendo todos los pokemons', error));
 }
 
- async function getOnePokemon(url) {
+async function getOnePokemon(url) {
   return fetch(url)
     .then((response) => response.json())
     .then((response) => {
@@ -71,16 +75,42 @@ const POKEMONS_INFO = [];
     .catch((error) => console.log('Error obteniendo pokemon individual', error));
 }
 
-function setTypeColors(p) {
 
-  p.style.backgroundColor = typeColors[p.textContent];
+
+function filterByName(list) {
+  const pokemonsFiltered$$ = [];
+  for (let pokemon of list) {
+    
+    if (pokemon.name.includes(searcher$$.value)) {
+      pokemonsFiltered$$.push(pokemon);
+    } 
+  }
+
+  
+  if(pokemonsFiltered$$.length === 0){
+
+    notFound$$.classList.add('notfound-display') 
+  } else {
+    notFound$$.classList.remove('notfound-display');
+  }
+
+
+  renderPokedex(pokemonsFiltered$$);
+
+}
+  
+function setTypeColors(type) {
+
+  type.style.backgroundColor = typeColors[type.textContent];
 
 }
 
 function renderPokedex(pokemons){
 
+  pokedexGrid$$.innerHTML = '';
+
   for (let pokemon of pokemons){
-    
+
     const card$$ = document.createElement('li');
     const pickContainer$$ = document.createElement('div');
     const infoContainer$$ = document.createElement('div');
@@ -88,7 +118,6 @@ function renderPokedex(pokemons){
     const name$$ = document.createElement('h2');
     const id$$ = document.createElement('p');
     const typesContainer$$ = document.createElement('div')
-    //const type$$ = document.createElement('p');
 
     card$$.classList.add('card')
     pickContainer$$.classList.add('pick-container');
@@ -120,41 +149,39 @@ function renderPokedex(pokemons){
 
       setTypeColors(typeName$$);
     }
-
-
   }
-
-
 }
-//? Creando opciones para meter en la lista de tipos
 
-  for (let type in typeColors) {
-    /* console.log('Bucle forin',type); */
+//? Creando opciones para meter en la lista de tipos
+const typeSelector$$ = document.querySelector('.types-selector');
+typeSelector$$.addEventListener('input', () => console.log(typeSelector$$.value));
+for (let type in typeColors) {
+    
     const typeOption$$ = document.createElement('option');
-    const typeSelector$$ = document.querySelector('.types-selector');
+    
     
     typeOption$$.textContent = type;
     typeOption$$.setAttribute('value', type);
+    
+    //typeOption$$.addEventListener('click', console.log(typeOption$$.value))
 
     typeSelector$$.appendChild(typeOption$$);
 
   }
 
-
-
+//* funcion que inicia la página
 async function init(){
   //?guardamos el valor de la funcion getAllPokemos (objeto) en una variable, e iteramos sobre el para 
   //?conseguir la url de cada pokemon
   const allPokemons = await getAllPokemons();
-  /* console.log(allPokemons); */
-  
-  for(let pokemon of allPokemons) {
+    for(let pokemon of allPokemons) {
     //?enviamos la url individual a la funcion con el segundo fetch y pusheamos su valor en un array para 
     //?guardar el objeto de cada pokemon
     let eachPokemon = await getOnePokemon(pokemon.url);
     POKEMONS_INFO.push(eachPokemon);
   }
-
+  
+  searcher$$.addEventListener('input', () => filterByName(POKEMONS_INFO))
   renderPokedex(POKEMONS_INFO);
 
 }
